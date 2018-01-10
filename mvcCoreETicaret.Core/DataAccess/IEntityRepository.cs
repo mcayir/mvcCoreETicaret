f09b3018456c5f74cdfbb693530f2d370c4f7216
mcayir <35219243+mcayir@users.noreply.github.com>
2018-01-10 12:35:54 +0300

    ilk yükleme

---
+++ mvcCoreETicaret.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using mvcCoreETicaret.Core.Entities;
+using System.Linq.Expressions;
+
+namespace mvcCoreETicaret.Core.DataAccess
+{
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        T Get(Expression<Func<T,bool>> filter=null);
+        List<T> GetList(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
