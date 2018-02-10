e98568a1f025fbab3ea287416a3bb222f405685f
mcayir <35219243+mcayir@users.noreply.github.com>
2018-02-10 03:18:31 +0300

    Revert "Sepetten Silme ve Sipariş Verme Yapıldı."
    This reverts commit c4dc9a2a93f38be60d183ac21a816decca5f6340.

--- mvcCoreETicaret.Northwind.MvcWebUI/Models/ShippingDetailsViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using mvcCoreETicaret.Northwind.Entities.Concrete;
-
-namespace mvcCoreETicaret.Northwind.MvcWebUI.Models
-{
-    public class ShippingDetailsViewModel
-    {
-        public ShippingDetails ShippingDetails { get; set; }
-    }
-}
